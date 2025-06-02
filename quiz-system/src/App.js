b3335e6794d950c52462031da5bcb1013535d758
Gaveen Amarasinghe <gaveenamarasinghe2004@gmail.com>
2025-06-02 18:11:01 +0530

    Update App.js

--- quiz-system/src/App.js
+++ quiz-system/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./Components/dashboard";
+import Login from "./Components/login";
+import Signup from "./Components/signup";
 
 
 
@@ -10,6 +12,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         
       </Routes>
     </Router>
